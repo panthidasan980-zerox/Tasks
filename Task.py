f1e9c814a85b0932df7847e3c89c00061b5a9510
panthidasan980-zerox <panthidasan980@gmail.com>
2026-03-08 10:34:59 -0400

    Task 3

--- Task.py
+++ Task.py
@@ -1,10 +1,17 @@
-person = {
-   "name": "Sita",
-   "marks": {
-       "math": 90,
-       "science": 85
+result = [
+   {
+       "emotion": {
+           "happy": 82.09,
+           "sad": 3.16,
+           "angry": 1.41
+       },
+       "dominant_emotion": "happy",
+       "confidence": 0.98
    }
-}
+]
 
-print(person["marks"]["science"])
-print(person["marks"]["math"])
+data = result[0]
+
+print(data["dominant_emotion"])
+print(data["emotion"]["happy"])
+print(data["confidence"])
